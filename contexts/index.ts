963b8dc71874fb6732af83295ce15f8a13798200
DishanH <dishan@outlook.com>
2025-09-27 15:32:06 -0400

    remove communty context from index

--- contexts/index.ts
+++ contexts/index.ts
@@ -7,4 +7,3 @@ export { ScrollContext, useScrollVisibility } from './ScrollContext';
 export { SettingsProvider, useSettings } from './SettingsContext';
 export { SubscriptionProvider, useSubscription, type SubscriptionPlan, type SubscriptionStatus, type PlanDetails, type UserSubscription } from './SubscriptionContext';
 export { ThemeProvider, useTheme } from './ThemeContext';
-export { CommunityProvider, useCommunity, type CommunityProfile, type DirectMessage, type CommunityNotification } from './CommunityContext'; 
